177de79cbe6f2e7b00cd7fc0df6388bd5b50568f
MattBoxes <31813312+MattBoxes@users.noreply.github.com>
2018-08-26 09:20:41 -0700

    added simple league button functions and toast

---
+++ app/src/main/java/com/example/matt/basketbalapp/SkillActivity.kt
@@ -0,0 +1,15 @@
+package com.example.matt.basketbalapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    var league = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
+    }
+}
---
+++ app/src/main/java/com/example/matt/basketbalapp/constants.kt
@@ -0,0 +1,3 @@
+package com.example.matt.basketbalapp
+
+const val EXTRA_LEAGUE = "league"
